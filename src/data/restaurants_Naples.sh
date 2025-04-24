ce5bf9c1f146aa4d467b325a881035d543bdb0aa
David Campo <dncampo@gmail.com>
2025-04-24 10:11:41 +0200

    Script adds extra entities.

---
+++ src/data/restaurants_Naples.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+curl --location 'http://localhost:1026/ngsi-ld/v1/entityOperations/upsert' \
+--header 'Content-Type: application/json' \
+-H 'Link: <http://context/ngsi-context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"' \
+-H 'Accept: application/ld+json' \
+-H 'fiware-service: ld' \
+-d @"${SCRIPTPATH}/generated_GPT4o_Naples_v1_extra_entities.json"
